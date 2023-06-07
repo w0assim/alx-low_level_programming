26ef2ccbf565ec9a02cf33038c4ef21690bc266a
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-07 07:02:50 -0700

    s

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,21 +1,18 @@
 #include "main.h"
 
-/*function printing on stdout
-string passed in arguments
-as a series of characters
-then adding a new line
-*/
+/*Function prints to stdout
+ string passed in arguments
+ as series of characters
+ then adding new line*/
 
 
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		s = s + 1;
-	}
-	else 
-	{
-		_putchar('\n');
-	}
+    while (*s)
+    {
+        _putchar(*s);
+        s = s + 1;
+    }
+    _putchar('\n');
 }
+
