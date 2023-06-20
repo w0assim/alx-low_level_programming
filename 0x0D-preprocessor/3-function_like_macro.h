e17d633af2b7a5849492a700c7cd6c8d4475ff5f
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-19 17:58:54 -0700

    s

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef _3_function_like_macro_h
-#define _3_function_like_macro_h
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
 #define ABS(x) (((x) > 0) ? (x) : (-x))
 #endif
 
