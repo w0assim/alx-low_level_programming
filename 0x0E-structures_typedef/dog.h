da21653f256d670b218c015ca3ba2d0f8b9129d0
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-19 18:27:27 -0700

    s

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
+#define < stdlib.h >
 
-struct dog {
-	char* name;
+/**
+ * struct dog - struct for a dog
+ * @name: name of dog
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+
+struct dog
+{
+	char *name;
 	float age;
-	char* owner;
-}
+	char *owner;
+};
 
+#endif
