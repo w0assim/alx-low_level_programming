4ecab3f7fa9cf75dd647f048a7b6ddbd827a2894
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-19 19:31:08 -0700

    s

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,11 +11,19 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *my_dog;
-
-	my_dog->name = name;
-	my_dog->age = age;
-	my_dog->owner = owner;
+	dog_t dog;
+	dog_t *my_dog=dog;
+	
+	if (my_dog != NULL)
+	{
+		my_dog->name = name;
+		my_dog->age = age;
+		my_dog->owner = owner;
+	}
+	else
+	{
+		return NULL;
+	}
 	return (my_dog);
 }
 
