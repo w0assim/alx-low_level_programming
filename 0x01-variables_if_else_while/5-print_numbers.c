f308d5c22d5af4230b1a435538a7b99f5aaea623
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-23 04:35:28 -0700

    wassim

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main -main
+ *Return: none
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 0;
+	while (x != 10)
+	{
+		printf("%d", x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
+
