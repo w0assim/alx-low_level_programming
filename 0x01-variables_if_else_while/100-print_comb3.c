9e07ea22e65a9cef41eb0e5a0af15e58be89d103
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-23 12:12:23 -0700

    wassim

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main -main function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int x, y;
+	
+	y = '0';
+	for (x = 0; x < 9; x++)
+	{
+		if (y == x)
+		{
+			y++;
+		}
+		while (y < 9 && y != x)
+		{
+			putchar(x);
+			putchar(y);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
