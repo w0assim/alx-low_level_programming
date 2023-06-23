5a4d83720525689e55aed83a328faa0f0e905034
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-23 04:31:35 -0700

    wassim

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,18 +7,18 @@
 
 int main(void)
 {
-        char c;
-
-        c = 'a';
-        while (c != ('z' + 1))
-        {
-                if (c != 'e' || c != 'q')
-                {
-                        putchar(c);
-                }
-                c++;
-        }  
-        putchar('\n');
-        return (0);
+	char c;
+	
+	c = 'a';
+	while (c != ('z' + 1))
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
 }
 
