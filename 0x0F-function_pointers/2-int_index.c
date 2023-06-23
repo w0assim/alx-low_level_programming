47e69545464505d33c28b720929413bc3d352766
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-23 09:12:59 -0700

    wassim

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,20 +10,30 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int x;
+	int x, i, cmpt;
 
 	if (array == NULL || cmp == NULL || size <= 0)
 	{
 		return (-1);
 	}
-	x = cmp(array);
-	if (x < 0 || x > (size - 1))
+	i = 0;
+	cmpt = -1;
+	while (i++ < size - 1)
+	{
+		x = cmp(array[i]);
+		if (x == 1)
+		{
+			cmpt = i;
+			break;
+		}
+	}
+	if (cmpt < 0 || cmpt > (size - 1))
 	{
 		return (-1);
 	}
 	else
 	{
-		return (x);
+		return (cmpt);
 	}
 }
 
