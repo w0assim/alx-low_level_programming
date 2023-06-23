f0eacf2697e55d2e363b5d76ec0d3aed617dc75e
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-23 13:04:31 -0700

    wassim

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ *main -test
+ *Return: 0
+ */
+
+int main(void)
+{
+	int x, y, z;
+
+	for (x = '0'; x < '8'; x++)
+	{
+		y = 1 + x;
+		z = 1 + y;
+		while (y < '9')
+		{
+			while (z < '9' + 1)
+			{
+				putchar(x);
+				putchar(y);
+				putchar(z);
+				if (x != '7' || y != '8' || z != '9')
+				{
+					putchar(',');
+					putchar(" ");
+				}
+				z++;
+			}
+			y++;
+		}
+	}
+	putchar('\n');
+}
+
