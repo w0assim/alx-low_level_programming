82413c5f01b9231b611721b211b5c55f052d6372
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-23 19:47:31 -0700

    wassim

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - main function
+ *Return: none
+ */
+
+int main(void)
+{
+	int a, b, c, c;
+
+	for (a = '0'; a < '9' + 1; a++)
+	{
+		for (b = '0'; b < '9' + 1; b++)
+		{
+			
