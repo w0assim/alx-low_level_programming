33b4403433eb47b471e45890c3017990745faee8
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-23 19:26:37 -0700

    wassim

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+
+/**
+ * op_add -operation
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub -operation
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul -operation
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div -operation
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod -operation
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
+
