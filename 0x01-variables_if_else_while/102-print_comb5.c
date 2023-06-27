f532df3515dc3f20e279bba6caa8cfcf442e5ea5
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-26 22:58:39 -0700

    wassim

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,10 +7,35 @@
 
 int main(void)
 {
-	int a, b, c, c;
+	int a, b, c, d;
 
 	for (a = '0'; a < '9' + 1; a++)
 	{
-		for (b = '0'; b < '9' + 1; b++)
+		for (b = '0'; b <= '9'; b++)
 		{
-			
+			c = a;
+			d = b + 1;
+			while (c != '9' + 1)
+			{
+				while (d != '9' + 1)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(" ");
+					putchar(c);
+					putchar(d);
+					if (c != 9 || d != 9)
+					{
+						putchar(",");
+						putchar(" ");
+					}
+					c++;
+				}
+				d++;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
