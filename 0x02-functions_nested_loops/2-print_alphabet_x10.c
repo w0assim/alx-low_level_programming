c87233521bfe8da559f9b99085e92f068d75236e
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-28 00:26:25 -0700

    wassim

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -alp
+ */
+
+void print_alphabet_x10(void)
+{
+	int cpt;
+	char c;
+
+	for (cpt = 0; cpt < 10; cpt++)
+	{
+		c = 'a';
+		while (c != 'z' + 1)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
+
