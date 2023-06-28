2a1f742f7c8aa1b7bc59f78386120f26ee63c0e8
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-28 01:25:49 -0700

    w

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs -absolute
+ * Return: absolute value
+ * @x: vslue
+ */
+
+int _abs(int x)
+{
+	int absolute;
+
+	absolute = abs(x);
+	return (absolute);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,14 @@
 #ifndef aaaa
 #define aaaa
 #include <ctype.h>
+#include <stdlib.h>
+
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int);
 
 #endif
