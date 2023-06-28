96156c0d6bcde4c35f1abfabd5ede1c3a0718f1a
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-28 01:29:57 -0700

    w

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit -last digit
+ * @x: number
+ * Return: last digit
+ */
+
+int print_last_digit(int x)
+{
+	int last_digit;
+
+	last_digit = x % 10;
+	return (last_digit);
+}
+
