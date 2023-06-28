8ff33c4506d0ce981e8f96ea61a280a1fe585994
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-28 03:01:13 -0700

    w

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * jack_bauer -time
+ */
+
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	a = '0';
+	while (a != '0' + 3)
+	{
+		b = '0';
+		while (b != '0' + 4)
+		{
+			c = '0';
+			while (c != '0' + 6)
+			{
+				d = '0';
+				while (d != '0' + 10)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					d++;
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
+
