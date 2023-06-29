53dc4e00091d6c03697488afae344741dcb580cc
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-29 09:53:23 -0700

    W

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	long long fib1, fib2;
+	long int fib1, fib2;
 
 	fib1 = 0;
 	fib2 = 1;
@@ -19,19 +19,19 @@ int main(void)
 		{
 			if (fib1 < 4000000)
 			{
-				printf("%lld", fib1);
+				printf("%ld", fib1);
 			}
 		}
 		else
 		{
-			print("%lld, ", fib1);
+			printf("%ld, ", fib1);
 			if ((fib1 + fib2) < 4000000)
 			{
-				printf("%lld, ", fib2);
+				printf("%ld, ", fib2);
 			}
 			else
 			{
-				printf("%lld", fib2);
+				printf("%ld", fib2);
 			}
 		}
 	}
