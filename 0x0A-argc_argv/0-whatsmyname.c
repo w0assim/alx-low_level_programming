1f925ca885ab393d2e5a61265b333fc354f7bbc3
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-29 16:12:02 -0700

    W

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main -,m
+ * Return: 0
+ * @argc: nbr of arg
+ * @argv: array of arg
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
