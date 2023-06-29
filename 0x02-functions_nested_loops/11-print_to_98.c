6b8c5add54869ab3e4bfcc7f5e76ac9020310a17
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-29 04:37:42 -0700

    w

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 -print to nb 98
+ * @n: starting nbr
+ */
+
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		if (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		else
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	printf("98");
+}
+
