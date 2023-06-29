e6ad02fcfb8c3c481245ae822fcd1f48b0f68acb
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-29 10:00:42 -0700

    w

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,35 +7,38 @@
 
 int main(void)
 {
-	long int fib1, fib2;
+	long int fib1, fib2, sum;
 
 	fib1 = 0;
 	fib2 = 1;
+	sum = 0;
 	while (fib1 < 4000000 && fib2 < 4000000)
 	{
 		fib1 += fib2;
 		fib2 += fib1;
-		if (fib2 >= 4000000)
+		if (fib2 < 4000000)
 		{
-			if (fib1 < 4000000)
+			if (fib2 % 2 == 0)
 			{
-				printf("%ld", fib1);
+				sum += fib2;
+			}
+			if (fib1 % 2 == 0)
+			{
+				sum += fib1;
 			}
 		}
 		else
 		{
-			printf("%ld, ", fib1);
-			if ((fib1 + fib2) < 4000000)
+			if (fib1 < 4000000)
 			{
-				printf("%ld, ", fib2);
-			}
-			else
-			{
-				printf("%ld", fib2);
+				if (fib1 % 2 == 0)
+				{
+					sum += fib1;
+				}
 			}
 		}
 	}
-	printf("\n");
+	printf("%ld\n", sum);
 	return (0);
 }
 
