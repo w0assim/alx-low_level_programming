959fc04bdc8b9af840f5c31319a8f080f905935e
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-29 09:22:05 -0700

    w

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Return: int
+ */
+
+int main(void)
+{
+	int cpt, fib1, fib2;
+
+	cpt = 0;
+	fib1 = 0;
+	fib2 = 1;
+	while (cpt != 50)
+	{
+		cpt++;
+		cpt++;
+		fib1 += fib2;
+		printf("%d, ", fib1);
+		fib2 += fib1;
+		if (cpt != 50)
+		{
+			printf("%d, ", fib2);
+		}
+		else
+		{
+			printf("%d\n", fib2);
+		}
+	}
+	return (0);
+}
+
