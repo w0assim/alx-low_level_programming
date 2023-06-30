5ac33f6475f4aff9dd16139a7c7d271561bac3c8
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-30 00:05:15 -0700

    w

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - ts
+ * Return: 0
+ * @argc: nbr of arg
+ * @argv: array of arg
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);;
+	return (0);
+}
+
