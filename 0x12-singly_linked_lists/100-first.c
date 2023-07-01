a873f7680de22f1d230bc547420550b26ae2ec42
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-07-01 16:27:36 -0700

    w

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,16 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * myStartupFun - Apply the constructor attribute to myStartupFun() so that it
- *   is executed before main()
- */
-void myStartupFun(void) __attribute__ ((constructor));
-
-/**
- * myStartupFun - implementation of myStartupFun
+ * beforemain - beforemain
  */
 
-void myStartupFun(void)
+void beforemain(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -18,10 +18,11 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-size_t print_list(const list_t*);
-size_t list_len(const list_t*);
-list_t *add_node(list_t**, const char*);
-list_t *add_node_end(list_t**, const char*);
-void free_list(list_t*);
+size_t print_list(const list_t *);
+size_t list_len(const list_t *);
+list_t *add_node(list_t **, const char *);
+list_t *add_node_end(list_t **, const char *);
+void free_list(list_t *);
+void beforemain(void) __attribute__ ((constructor));
 
 #endif
