00778298b833669f6efa2e3bf879ca796869c96b
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-04 14:43:18 +0100

    wass

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,22 +13,24 @@ int main(void)
 	unsigned long fib1=1, fib2=2, cpt=1, fib11, fib12, fib21, fib22;
 	while (cpt < 48)
 	{
-		while(fib1 < 100000000000000000 && fib2 < 100000000000000000)
+		while(fib1 < 1000000000000000000 && fib2 < 1000000000000000000)
 		{
 			printf("%lu, %lu, ", fib1, fib2);
 			fib1 += fib2;
 			fib2 += fib1;
 			cpt ++;
 		}
-		fib11 = fib1 / 10000000000;
-		fib12 = fib1 % 10000000000;
-		fib21 = fib2 / 10000000000;
-		fib22 = fib2 % 10000000000;
+		fib11 = fib1 / 10000000000000;
+		fib12 = fib1 % 10000000000000;
+		fib21 = fib2 / 10000000000000;
+		fib22 = fib2 % 10000000000000;
 		while (cpt < 48)
 		{
 			printf("%lu%lu, %lu%lu, ", fib11, fib12, fib21, fib22);
-			fib11 += fib21 + ((fib12 + fib22) / 10000000000);
-			fib12 = (fib12 + fib22) % 10000000000;
+			fib11 += fib21 + ((fib12 + fib22) / 10000000000000);
+			fib12 = (fib12 + fib22) % 10000000000000;
+			fib21 += fib11 + ((fib12 + fib22) / 10000000000000);
+			fib22 = (fib12 + fib22) % 10000000000000;
 			cpt++;
 		}
 	}
