c513d161329f494c657f967eff6d4a4cb86c6b5b
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-04 10:20:50 +0100

    w

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,93 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - main
+ * main - main 
  * Return: int
  */
 
 int main(void)
 {
-	unsigned long fib1, fib2, cpt, fib11, fib12, fib21, fib22;
-
-	cpt = 0;
-	fib1 = 0;
-	fib2 = 1;
-	while (cpt < 98)
-	{
-		fib11 = fib1 / 1000000;
-		fib12 = fib1 % 1000000;
-		fib21 = fib2 / 1000000;
-		fib22 = fib2 % 1000000;
-		if (fib1 >= 2000000000 || fib2 >= 2000000000)
-		{
-			if (fib1 < fib2)
-			{
-				while (cpt < 98)
-				{
-					fib11 += fib21;
-					fib12 += fib22;
-					fib11 += fib12 / 1000000;
-					fib12 %= 1000000;
-					cpt++;
-					if (cpt < 98)
-					{
-						printf("%ld%ld, ", fib11, fib12);
-						fib21 += fib11;
-						fib22 += fib12;
-						fib21 += fib22 / 1000000;
-						fib22 %= 1000000;
-						cpt++;
-						if (cpt <98)
-							printf("%ld%ld, ", fib21, fib22);
-						else
-							return (1);
-					}
-					else
-						printf("%ld%ld", fib11, fib12); return (1);
-				}
-			}
-			else
-			{
-				while (cpt < 98)
-				{
-					fib21 += fib11;
-					fib22 += fib12;
-					fib21 += fib22 / 1000000;
-					fib22 %= 1000000;
-					cpt++;
-					if (cpt < 98)
-					{
-						printf("%ld%ld, ", fib21, fib22);
-						fib11 += fib21;
-						fib12 += fib22;
-						fib11 += fib12 / 1000000;
-						fib12 %= 1000000;
-						cpt++;
-						if (cpt < 98)
-							printf("%ld%ld, ", fib11, fib12);
-						else
-							return (1);
-					}
-					else
-						printf("%ld%ld", fib21, fib22); return (1);
-				}
-			}
-		}
-		else
-		{
-			if (fib1 < fib2)
-			{
-				if (cpt == 0)
-					printf("%ld, %ld, ", fib1, fib2);
-				fib1 += fib2;
-				cpt++;
-				printf("%ld, ", fib1);
-			}
-			else
-			{
-				fib2 += fib1; cpt++; printf("%ld, ", fib2);
-			}
-		}
+	unsigned int fib1=1, fib2=2;
+	int cpt=1;
+	printf("%u, %u\n", fib1, fib2);
+	fib1 += fib2;
+	fib2 += fib1;
+	while (cpt < 98){
+		printf("%u, %u", fib1, fib2);
+		fib1 += fib2;
+		fib2 += fib1;
+		cpt ++;
 	}
 }
-
