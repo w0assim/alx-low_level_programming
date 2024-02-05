79bfb79694f2d7599712d29b20a2fa96dccf5bc8
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-05 21:19:12 +0100

    wass

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-	unsigned long fib1 = 1, fib2 = 2, cpt = 1, fib11, fib12, fib21, fib22;
+	unsigned long fib1 = 1, fib2 = 2, cpt = 1, fib11, fib12, fib21, fib22, output_fib11, output_fib21;
 
 	while (cpt <= 48)
 	{
@@ -24,26 +24,47 @@ int main(void)
 		fib12 = fib1 % 10000000000000;
 		fib21 = fib2 / 10000000000000;
 		fib22 = fib2 % 10000000000000;
+		printf("%lu%lu, %lu%lu, ", fib11, fib12, fib21, fib22);
 		while (cpt <= 48)
 		{
-			printf("%lu%lu, %lu%lu, ", fib11, fib12, fib21, fib22);
 			fib11 += fib21 + ((fib12 + fib22) / 10000000000000);
 			if ((((fib12 + fib22) / 1000000000000) % 10 == 0)
 			&& ((fib12 + fib22) / 1000000000000) != 0)
 			{
-				fib11 *= 10;
+				output_fib11 = fib11 * 10;
+			}
+			else
+			{
+				output_fib11 = fib11;
 			}
 			fib12 = (fib12 + fib22) % 10000000000000;
 			fib21 += fib11 + ((fib12 + fib22) / 10000000000000);
 			if ((((fib12 + fib22) / 1000000000000) % 10 == 0)
 			&& ((fib12 + fib22) / 1000000000000) != 0)
 			{
-				fib21 *= 10;
+				output_fib21 = fib21 * 10;
+			}
+			else
+			{
+				output_fib21 = fib21;
 			}
 			fib22 = (fib12 + fib22) % 10000000000000;
+			printf("%lu%lu, %lu%lu, ", output_fib11, fib12, output_fib21, fib22);
 			cpt++;
 		}
 	}
 	printf("%lu%lu, %lu%lu \n", fib11, fib12, fib21, fib22);
 	return (1);
 }
+
+
+//      10000000000000
+// 1220016,0415121876738
+//  754011,3804746346429
+//12200160,415121876738
+// 7540110,3804746346429
+
+
+//129541714219868223167
+// 19740274219868223167
+// 		 3804746346429
