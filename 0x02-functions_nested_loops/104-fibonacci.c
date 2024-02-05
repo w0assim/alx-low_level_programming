d434867c3ae8b8c9ed8f5a552bda9196e3182f51
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-05 20:57:50 +0100

    wass

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
+
 /**
  * main - main
  * Return: int
  */
-/**
- * 18446744073709551615
- * 1000000000000000000
- */
+
+
 int main(void)
 {
 	unsigned long fib1 = 1, fib2 = 2, cpt = 1, fib11, fib12, fib21, fib22;
@@ -29,8 +28,18 @@ int main(void)
 		{
 			printf("%lu%lu, %lu%lu, ", fib11, fib12, fib21, fib22);
 			fib11 += fib21 + ((fib12 + fib22) / 10000000000000);
+			if ((((fib12 + fib22) / 1000000000000) % 10 == 0)
+			&& ((fib12 + fib22) / 1000000000000) != 0)
+			{
+				fib11 *= 10;
+			}
 			fib12 = (fib12 + fib22) % 10000000000000;
 			fib21 += fib11 + ((fib12 + fib22) / 10000000000000);
+			if ((((fib12 + fib22) / 1000000000000) % 10 == 0)
+			&& ((fib12 + fib22) / 1000000000000) != 0)
+			{
+				fib21 *= 10;
+			}
 			fib22 = (fib12 + fib22) % 10000000000000;
 			cpt++;
 		}
--- 0x02-functions_nested_loops/tmp.c
+++ 0x02-functions_nested_loops/tmp.c
@@ -14,5 +14,5 @@ int main(void)
     unsigned long i =     1231231231231231234;
     unsigned long d = i / 10000000000000000;
     unsigned long c = i % 10000000000000000;
-    printf("%lu%lu", d, c);
+    printf("%d", 0%10);
 }
