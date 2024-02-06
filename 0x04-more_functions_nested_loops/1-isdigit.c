0f0023278ba37e8f7ff3299873922c39a0ac84a0
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-06 21:56:01 +0100

    wass

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,20 @@
-
+#include <ctype.h>
 #include "main.h"
 
 /**
- * @brief 
- * 
+ * _isdigit - check if digit
+ * @c: char to check
+ * Return: 0/1
  */
+
+int _isdigit(int c)
+{
+	if (!(isdigit(c)))
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
