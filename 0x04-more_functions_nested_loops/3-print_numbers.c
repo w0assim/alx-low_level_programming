d0fcea6c0ed718d41419797a5b4a7b3212714649
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-06 22:03:40 +0100

    wass

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print nb 0 to 9
+ * Return: none
+ */
+
+void print_numbers(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
