a6fab6349de306232b1428da336ec79fc4fb9ef0
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-07 12:47:01 +0100

    wass

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle shape in terminal 
+ * @size: height of triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, j, m;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = i; j <= size; j++)
+		{
+			_putchar(' ');
+		}
+		for (m = 1; m <= i; m++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (i == 1)
+	{
+		_putchar('\n');
+	}
+}
