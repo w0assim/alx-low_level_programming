db760fa83c71d332b38eaae907a69dd7cf86c03f
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-07 13:23:45 +0100

    wass

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - prime factor
+ * Return: 0
+ */
+
+int main(void)
+{
+	int max = 0, i;
+
+	for (i = 2; i <= sqrt(612852475143); i++)
+	{
+		if (i % 612852475143 == 0)
+		{
+			if (i >= max)
+			{
+				max = i;
+			}
+		}
+	}
+	printf("%d\n", max);
+}
