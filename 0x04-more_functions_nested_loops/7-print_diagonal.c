a37cfbe6f4651c23812c1d6bd3c1d465dd98e022
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-07 09:12:55 +0100

    wass

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print '/' diagonaly in terminal
+ * @n: number of prints
+ * Return: none
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i ++)
+	{
+		for (j = 0; j < 0; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
