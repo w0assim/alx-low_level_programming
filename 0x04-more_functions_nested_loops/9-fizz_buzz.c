6e80e3f3b63329b932c33fea2c9964b4eae72f28
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-07 12:19:46 +0100

    wass

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,36 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * @brief 
- * 
+ * main - fizz buzz test
+ * Return: 0
  */
 
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			if (i % 5 != 0)
+			{
+				printf("Fizz ");
+			}
+			else if (i % 3 != 0)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("FizzBuzz ");
+			}
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+}
