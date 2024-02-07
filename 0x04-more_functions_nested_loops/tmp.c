6608d45dace993245696aab284364f52bbf4221f
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-07 09:01:59 +0100

    wass

--- 0x04-more_functions_nested_loops/tmp.c
+++ 0x04-more_functions_nested_loops/tmp.c
@@ -1,13 +1,19 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+//#include "6-print_line.c"
+
+
+int _putchar(int c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
 
 int main(void)
 {
-	for (int i = 0, j = 0; j < 10; i++)
-	{
-		j = i;
-		printf("%d", j);
-	}
-	//printf("%d", (isupper(c)));
+	print_line(-3);
 }
