2d25597275488b403f4629acdeac83b44abd0d2d
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-07 09:05:12 +0100

    wass

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,21 +9,25 @@
 void more_numbers(void)
 {
 	int i, j, y;
-	for (i = 48, y = 0; y < 10; i++, y++)
+	for (i = 48, y = 0; y < 10; i++)
 	{
-		for (j = 48; j < 52; j++)
+		for (j = 48; j < 53; j++)
 		{
 			_putchar(i);
 			if (i == 57 || j != 48)
 			{
 				i = 49;
+				_putchar(j);
 			}
 			else
 			{
 				break;
 			}
-			_putchar(j);
 		}
-		_putchar('\n');
+		if (j == 52)
+		{
+			_putchar('\n');
+			y++;
+		}
 	}
 }
