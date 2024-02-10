af2da48fb786fbadc356be135989508a9e885609
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-10 20:22:40 +0100

    wass

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - largest prime divider
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long i, target = 612852475143;
+
+	for (i = 2; i <= target; i++)
+	{
+		if (target % i == 0)
+		{
+			while (target % i == 0)
+			{
+				target /= i;
+			}
+			if (target == 1)
+			{
+				printf("%lu\n", i);
+			}
+		}
+	}
+}
