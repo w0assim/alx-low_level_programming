5582dcc6b030cc1c3bbd76e9383057bfc46dd2f5
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-13 10:58:16 +0100

    wass

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update value pointed to by pointer
+ * @n: pointer
+ * Return: none
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef main__H
+#define main__H
+
+int _putchar(int c);
+void reset_to_98(int *n);
+
+
+#endif
