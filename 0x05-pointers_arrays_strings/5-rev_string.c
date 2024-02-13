d17bc240cc48c0a84537672d2afcd879cdf70f4b
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-13 11:28:55 +0100

    wass

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverse a string
+ * @s: string
+ * Return: none
+ */
+
+void rev_string(char *s)
+{
+	char t;
+	int i, j;
+
+	for (i = 0, j = strlen(s); i >= j; i++, j--)
+	{
+		t = s[i];
+		s[i] = s[j];
+		s[j] = t;
+	}
+}
