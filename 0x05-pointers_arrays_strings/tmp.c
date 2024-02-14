d4a6f2a257d3a6fc6d0dddd62335cddbaa710e8a
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-14 17:08:51 +0100

    wass

--- 0x05-pointers_arrays_strings/tmp.c
+++ 0x05-pointers_arrays_strings/tmp.c
@@ -3,12 +3,11 @@
 #include "5-rev_string.c"
 #include <string.h>
 #include <math.h>
-
+#include "7-puts_half.c"
 
 int main(void)
 {
 	int i = 5;
 
-	//printf("%lf", ceil(i / 2));
-	printf("%lf", ceil((double)5 / 2));
+	puts_half("hello");
 }
