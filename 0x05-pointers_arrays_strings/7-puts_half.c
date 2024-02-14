c1eb167305e12c28ccc14a072d8a0a00246cd60c
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-14 17:19:54 +0100

    wass

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,7 +16,7 @@ void puts_half(char *str)
 	}
 	if (i % 2 == 0)
 	{
-		j = (i - 1) / 2;
+		j = i / 2;
 	}
 	else
 	{
@@ -26,4 +26,5 @@ void puts_half(char *str)
 	{
 		putc(str[j], stdout);
 	}
+	putc('\n', stdout);
 }
