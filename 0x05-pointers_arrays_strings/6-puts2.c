69a9782a50077691f299b06ed8e7c1461b4f8f24
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-14 10:30:21 +0100

    wass

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * puts2 - 
- * 
+ * puts2 - print every other character on string
+ * @str: string to print
+ * Return: none
  */
 
 void puts2(char *str)
-{}
+{
+	int i, j;
+
+	for (i = 0, j = 0; i != '\0' && j != '\0'; i += 2, j++)
+	{
+		putc(str[i], stdin);
+		j = i;
+	}
+}
