394de37754e6ba7e237c000b00c80911ae1fac60
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-15 23:40:31 +0100

    wass

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,9 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_array - print
- * 
+ * print_array - print n nbrs from arrat
+ * @n: number of nbrs to be printed
+ * @a: array of ints
+ * Return: none
  */
 
 void print_array(int *a, int n)
-{}
+{
+	int i = 0;
+
+	while (n - i < 0)
+	{
+		printf("%d", a[i]);
+		i++;
+	}
+}
