c56fd5a3083a9696ba42105157e3534f7f3e1944
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-28 19:46:35 +0100

    wass

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,10 +11,10 @@ void reverse_array(int *a, int n)
 {
 	int i, tmp;
 
-	for (i = n; n - i < i; i--)
+	for (i = n - 1; n - 1 - i < i; i--)
 	{
 		tmp = a[i];
-		a[i] = a[n-i];
-		a[n-i] = tmp;
+		a[i] = a[n-1-i];
+		a[n-1-i] = tmp;
 	}
 }
