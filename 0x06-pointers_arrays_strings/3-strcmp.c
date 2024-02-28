0e6a51182ac4e51442868b7adcba5f606d9d3bba
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-28 18:20:04 +0100

    wass

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+#include <string.h>
+
+/**
+ * _strcmp - compare strings
+ * @s1: first string
+ * @s2: second string
+ * Return: return of libc strcmp
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return(strcmp(s1, s2));
+}
