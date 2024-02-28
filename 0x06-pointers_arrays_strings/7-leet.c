ca8f106caec16b09aa3990a730537dfbe9189c4c
w0assim <wassim.ouanaou@hotmail.fr>
2024-02-28 21:16:45 +0100

    wass

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,9 +1,38 @@
 #include "main.h"
 
 /**
- * leet - 
- * 
+ * leet - turn string into leet notation
+ * @s: string to operate
+ * Return: string modified
  */
 
-char *leet(char *)
-{}
+char *leet(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == 'a' || s[i] == 'A')
+		{
+			s[i] = '4';
+		}
+		else if (s[i] == 'l' || s[i] == 'L')
+		{
+			s[i] = '1';
+		}
+		else if (s[i] == 'o' || s[i] == 'O')
+		{
+			s[i] = '0';
+		}
+		else if (s[i] == 'e' || s[i] == 'E')
+		{
+			s[i] = '3';
+		}
+		else if (s[i] == 't' || s[i] == 'T')
+		{
+			s[i] = '7';
+		}
+		i ++;
+	}
+	return (s);
+}
