787acfd0286c90a6c31cac44305400bff1b6491f
w0assim <wassim.ouanaou@hotmail.fr>
2024-03-03 10:02:48 +0100

    wass

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - search for character in string
+ * @c: character to find
+ * @s: string to search
+ * Return: pointer to string
+ */
+
+char *_strchr(char *s, char c)
+{
+	return(strchr(s, c));
+}
